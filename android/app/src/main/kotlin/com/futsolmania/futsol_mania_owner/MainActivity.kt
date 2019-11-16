3336d7d411a7686e313b177afb979765e06f1a9b
Muhammad_Suleman <msuleman526@gmail.com>
2019-11-16 21:24:18 +0500

    Futsol Mania Owner App

---
+++ android/app/src/main/kotlin/com/futsolmania/futsol_mania_owner/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.futsolmania.futsol_mania_owner
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/Database/Firebase.dart
@@ -0,0 +1,10 @@
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:futsol_mania_owner/Models/User.dart';
+import 'package:futsol_mania_owner/utils/User_Defaults.dart';
+
+class Firebase_Data{
+
+   static String uid;
+
+}
